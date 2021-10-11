2a96720c2c911669135dadfbe2f04f22304af583
kooooo2014 <20141763kooooo@gmail.com>
2021-10-11 10:12:29 +0900

    homeコンポーネントのパスを変更

--- src/router/index.js
+++ src/router/index.js
@@ -6,27 +6,9 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: './',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/Profile',
-    name: 'Profile',
-    component: () => import('../views/Profile.vue')
-  },
-  {
-    path: '/Contact',
-    name: 'Contact',
-    component: () => import('../views/Contact.vue')
   }
 ]
 
